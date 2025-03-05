3f14fc2ea47e1b625a441ffab985c078dc9e6213
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-03-05 05:04:42 +0000

    Fixed grammar errors.

--- BoardFoot.java
+++ BoardFoot.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 /** .
-* This calculates .
+* This calculates the length of a board foot.
 *
 * @author  Zak Goneau
 * @version 1.0
@@ -45,13 +45,13 @@ public final class BoardFoot {
         // Create scanner object
         Scanner scanner = new Scanner(System.in);
 
-        // Assign user input to variable
+        // Assign user width to variable
         widthString = scanner.nextLine();
 
-        // Get height
+        // Get height from user
         System.out.println("Please enter a height(in): ");
 
-        // Assign user input to variable
+        // Assign user height to variable
         heightString = scanner.nextLine();
 
         // try converting height to double
@@ -60,26 +60,26 @@ public final class BoardFoot {
             // Try casting input to double and assigning to variable
             height = Double.parseDouble(heightString);
 
-            // Check if guess is out of range
+            // Check if height is negative
             if (height < 0) {
 
                 // If out of range tell user
                 System.out.println(height + " cannot be negative");
 
-                // Otherwise try casting width
+            // Otherwise try casting width
             } else {
 
                 // Try casting width to double
                 try {
                     width = Double.parseDouble(widthString);
 
-                    // Check if number is negative
+                    // Check if width is negative
                     if (width < 0) {
 
-                        // Tell user number is negative
+                        // Tell user width is negative
                         System.out.println("No negative width: " + width);
 
-                        // Call function to calculate length
+                    // Otherwise call function to calculate length
                     } else {
                         lengthResult = calculateLength(width, height);
 
@@ -91,14 +91,14 @@ public final class BoardFoot {
                         System.out.println("in");
                     }
 
-                    // Catch invalid width and tell user
+                // Catch invalid width and tell user
                 } catch (NumberFormatException error) {
                     System.out.println("Please enter a valid input. "
                             + error.getMessage());
                 }
             }
 
-            // Catch invalid height and tell user
+        // Catch invalid height and tell user
         } catch (NumberFormatException error) {
             System.out.println("Please enter a valid input. "
                     + error.getMessage());
@@ -107,13 +107,14 @@ public final class BoardFoot {
         // Close scanner
         scanner.close();
     }
-    // Function to calculate length
+    // Declare function to calculate length
     public static double calculateLength(final double width,
-             final double height) {
+            final double height) {
+
         // Declare volume
         double volume = 144;
 
-        // Calculate length and return
+        // Calculate length and return value
         return volume / (width * height);
     }
 }
